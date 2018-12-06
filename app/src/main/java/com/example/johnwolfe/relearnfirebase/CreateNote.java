c39f0829ba0ea3adcd850f5b66bffb523262c840
John <john.wolfe@funeraldirectorslife.com>
2018-12-05 23:10:49 -0600

    Adding createNote layout. DB schema setup. No controller functionality yet.

---
+++ app/src/main/java/com/example/johnwolfe/relearnfirebase/CreateNote.java
@@ -0,0 +1,59 @@
+package com.example.johnwolfe.relearnfirebase;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class CreateNote extends AppCompatActivity {
+
+    private static Button submitNote;
+    private static EditText noteText;
+
+    private FirebaseDatabase mDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference mRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+
+        submitNote = (Button) findViewById(R.id.submitNote);
+        noteText = (EditText) findViewById(R.id.noteText);
+
+        // Must be signed in to use
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference();
+
+        submitNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitNoteToDb(view);
+            }
+        });
+    }
+
+    public void submitNoteToDb(View view){
+
+
+    }
+
+    public void toast(String message){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, message, duration);
+        toast.show();
+    }
+}
--- app/src/main/java/com/example/johnwolfe/relearnfirebase/ToDoHome.java
+++ app/src/main/java/com/example/johnwolfe/relearnfirebase/ToDoHome.java
@@ -1,8 +1,8 @@
 package com.example.johnwolfe.relearnfirebase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -29,8 +29,8 @@ public class ToDoHome extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(view.getContext(), CreateNote.class);
+                startActivity(intent);
             }
         });
 
